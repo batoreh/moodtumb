5836c36bcbabfa5b7d7ca2993e845735a70e38ef
netoguimaraes <joao.neto.guimaraes@gmail.com>
2020-04-26 17:48:01 -0300

    demosite

---
+++ precache-manifest.aabca8f4e9b7e1428b6d3ca47eb3d40c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df9de5d7a9a6599f44933c97b3334b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d150b7d11b1407f218e1",
+    "url": "/static/css/2.7b86f385.chunk.css"
+  },
+  {
+    "revision": "7f42036bb72688696a9e",
+    "url": "/static/css/main.fd71b0bc.chunk.css"
+  },
+  {
+    "revision": "d150b7d11b1407f218e1",
+    "url": "/static/js/2.5625d182.chunk.js"
+  },
+  {
+    "revision": "7cd3cf7d6011ab2c4273866148250055",
+    "url": "/static/js/2.5625d182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f42036bb72688696a9e",
+    "url": "/static/js/main.784d6689.chunk.js"
+  },
+  {
+    "revision": "07b401fa6e6655381b41",
+    "url": "/static/js/runtime-main.2bf95fa4.js"
+  }
+]);
